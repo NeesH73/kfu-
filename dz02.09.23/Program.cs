b1d6221190ad7b2518f081ff39dbf52281a3ed20
Екатерина <@EVE_15_P417>
2023-09-05 16:44:33 +0300

    Добавьте файлы проекта.

---
+++ dz02.09.23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz02._09._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
